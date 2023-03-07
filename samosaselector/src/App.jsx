e30ac1b33a455802025f990422db7ac1c9e73773
jer <32397605+jerxmyg@users.noreply.github.com>
2023-03-06 23:17:41 -0500

    Implelmented count function in App.jsx; included pointer to in App.css

--- samosaselector/src/App.jsx
+++ samosaselector/src/App.jsx
@@ -1,17 +1,33 @@
 import './App.css';
+import { useState } from 'react';
 import samosa from "./assets/My project-1.png";
+
+
 const App = () => {
+  const [count, setCount] = useState(0);
+  const [multiplier, setMultiplier] = useState(1);
+
+  const updateCount = () => setCount(count + multiplier);
 
   return (
+    
     <div className="App">
       <div className="header">
+      
         <h1> Samosa Selector </h1>
-        <h2> Count: </h2>
-        <img className="samosa" src={samosa} />
+        <h2> Count: {count} </h2>
+        <img src={samosa} className="samosa" onClick={updateCount} /> 
+       
       </div>
+     
 
     </div>
+    
   )
+
+
+  
 }
+
 
 export default App
