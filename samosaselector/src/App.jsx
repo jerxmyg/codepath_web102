8f5b20fbb89197ce349dc9ef4c0512739491a0fa
jer <32397605+jerxmyg@users.noreply.github.com>
2023-03-06 22:30:07 -0500

    Added the title, count, and adjusted the size of the samosa.png

--- samosaselector/src/App.jsx
+++ samosaselector/src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css';
-
+import samosa from "./assets/My project-1.png";
 const App = () => {
 
   return (
     <div className="App">
+      <div className="header">
+        <h1> Samosa Selector </h1>
+        <h2> Count: </h2>
+        <img className="samosa" src={samosa} />
+      </div>
 
     </div>
   )
