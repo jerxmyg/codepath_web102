aea17db335ad237244868008f5e862e9dfb61bc9
jer <32397605+jerxmyg@users.noreply.github.com>
2023-03-06 23:28:41 -0500

    Implemented the descriptions for the upgrades.

--- samosaselector/src/App.jsx
+++ samosaselector/src/App.jsx
@@ -12,12 +12,44 @@ const App = () => {
   return (
     
     <div className="App">
+
+      
       <div className="header">
       
         <h1> Samosa Selector </h1>
         <h2> Count: {count} </h2>
         <img src={samosa} className="samosa" onClick={updateCount} /> 
        
+      </div>
+
+      <div className="container">
+
+        <div className="upgrade">
+
+          <h3> Double Stuffed👯‍♀️ </h3>
+          <p> 2x per click </p>
+          <button> 10 samosas </button>
+
+        </div>
+
+        <div className="upgrade">
+
+          <h3> Party Pack🎉 </h3>
+          <p> 5x per click </p>
+          <button> 100 samosas </button>
+
+        </div>
+
+        <div className="upgrade">
+
+          <h3> Full Feast👩🏽‍🍳 </h3>
+          <p> 10x per click </p>
+          <button> 1000 samosas </button>
+
+        </div>
+
+
+
       </div>
      
 
