f64b3626d7fabe55aaaf40df083a750db743d6a2
jer <32397605+jerxmyg@users.noreply.github.com>
2023-03-14 01:42:01 -0400

    Added arrows and shuffle feature. Also updated barista-app.

--- barista-app/src/Components/baristaForm.jsx
+++ barista-app/src/Components/baristaForm.jsx
@@ -1,6 +1,11 @@
 import React, {Component, useState} from "react";
 import RecipeChoices from "./recipeChoices";
+import drinksJson from "./drinks.json"
 const BaristaForm = () => {
+    const [currentDrink, setCurrentDrink] = useState('');
+  
+    const [trueRecipe, setTrueRecipe] = useState({});
+
     const [inputs, setInputs] = useState ({
         'tempature': ' ',
         'milk': ' ',
@@ -21,7 +26,16 @@ const BaristaForm = () => {
   return (
     <div>
         <h2>Hi, I'd like to order a:</h2>
+        <div className="drink-container">
+        <h2 className="mini-header">{currentDrink}</h2>
+        <button
+            type="new-drink-button"
+            className="button newdrink"
+            onClick={onNewDrink}
+        >
         
+        </button>
+        </div>
         
 
         <form >
@@ -102,6 +116,21 @@ const BaristaForm = () => {
 };
 
 const onNewDrink = () => {
+    setInputs({
+        'temperature': '',
+        'milk': '',
+        'syrup': '',
+        'blended': '' });
+        
+      getNextDrink();
+
+};
+
+const getNextDrink = () => {
+    let randomDrinkIndex = Math.floor(Math.random() * drinksJson.drinks.length);
+
+    setCurrentDrink(drinksJson.drinks[randomDrinkIndex].name);
+    setTrueRecipe(drinksJson.drinks[randomDrinkIndex].ingredients);
 
 };
 
--- flashcards/src/App.jsx
+++ flashcards/src/App.jsx
@@ -1,17 +1,116 @@
-import { useState } from 'react'
-import Display from './components/Display'
+import React from "react";
 import './App.css'
+import arrow from './assets/arrow.png'
+import backArrow from './assets/backArrow.png'
+import Flashcard from './components/Flashcards.jsx';
 
-function App() {
 
-  return (
+
+
+const App = () => {
+
+  
+  
+  const ok = () => {
+    console.log()
+  }
+
+  const Flashcards = [
+    <Flashcard question="The Bronx is the only borough of New York City which is believed to be part of the United States mainland." answer="True" />,
+
+    <Flashcard question="The major sports arena located in the Bronx is..." answer="Yankee Stadium"/>,
+
+    <Flashcard question="Which famous university calls The Bronx its home?" answer="Fordham University"/>,
+
+    <Flashcard question="The Bronx boasts its own beach which faces Long Island Sound. What is the name of this well known beach area?" answer="Orchard Beach"/>,
+
+    <Flashcard question="What is probably one of the most famous zoos in the world is located here. It's called The Bronx Zoo." answer="True"/>,
+
+    <Flashcard question="Which park, which is the fourth largest in the city, is located in the Northern Bronx, along the border with Westchester County?" answer="Van Cortlandt Park"/>,
+
+    <Flashcard question="This amusement park which was once located in the Bronx, is no longer in existence. Name it!" answer="Freedomland"/>,
+
+    <Flashcard question="There is a Hall Of Fame for Great Americans located in The Bronx." answer="True"/>,
+
+    <Flashcard question="What Bronx high school has the most Nobel Prize winners?" answer="Bronx Science"/>,
+
+    <Flashcard question="What two letters are an abbreviation for the Bronx?" answer="BX"/>
+  ]
+
+  const [card, changeFlashcard] = React.useState(Flashcards[Math.floor(Math.random()*Flashcards.length)])
+
+  const [text, changeText] = React.useState(card.props.question)
+
+  console.log(card.props.question)
+  const question = card.props.question
+  const answer = card.props.answer 
+
+  
+
+  const cardClick = () => {
+
+    if (text===question){
+      changeText(answer)
+    }
+
+    else if (text===answer){
+      changeText(question)
+    }
+  }
+
+  const shuffle = () => {
+    
+    const newCard = Flashcards[Math.floor(Math.random()*Flashcards.length)]
+    
+    changeText(newCard.props.question)
+    changeFlashcard(newCard)
+  }
+
+  const forward = () => {
+    
+    const newCard = Flashcards[Math.floor(Math.random()*Flashcards.length)]
+    
+    changeText(newCard.props.question)
+    changeFlashcard(newCard)
+  }
+
+  const back = () => {
+    
+    const newCard = Flashcards[Math.floor(Math.random()*Flashcards.length)]
+    
+    changeText(newCard.props.question)
+    changeFlashcard(newCard)
+  }
+  
+
+  return(
 
     <div className="App">
-
-      <Display/>
       
+      <div className="information">
+
+        <h3>The Bronx Trivia</h3>
+        <h6>How much do you know about The Bronx? Find out through these questions!</h6>
+        <h6>Number of cards: {Flashcards.length}</h6>
+
+      </div>
+
+      <div onClick={cardClick} className="flashcards">
+
+        {text}
+
+      </div>
+
+        <button onClick={back} className="backArrow"> <img src={backArrow} width="40px"/> </button>
+
+        <button onClick={forward} className="forwardArrow"> <img src={arrow} width="40px"/> </button>
+        
+        <button onClick={shuffle} className="shuffleButton"> Shuffle </button>
+
+
     </div>
   )
 }
 
-export default App
+export default App;
+
