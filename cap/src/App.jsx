1d4060f82e8c1856dcdd97a4ae56d2e3216a296d
jer <32397605+jerxmyg@users.noreply.github.com>
2023-03-20 23:21:50 -0400

    Step: 0.

---
+++ cap/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+  )
+}
+
+export default App
