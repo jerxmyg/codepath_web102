50ea7cfd05dd2aff940cdccb813ba9bc41f0c44d
jer <32397605+jerxmyg@users.noreply.github.com>
2023-03-26 19:06:11 -0400

    Step 0: Set up.

---
+++ crypto-hustle/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+  )
+}
+
+export default App
