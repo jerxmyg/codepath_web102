9bcf6e2dc7aaca121b0e172513e866f6f133bca2
jer <32397605+jerxmyg@users.noreply.github.com>
2023-03-26 18:59:09 -0400

    Updating other random projects.

--- cap/src/App.jsx
+++ cap/src/App.jsx
@@ -4,6 +4,15 @@ import './App.css'
 
 const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
 
+const [inputs, setInputs] = useState({
+  url: "",
+  format: "",
+  no_ads: "",
+  no_cookie_banners: "",
+  width: "",
+  height: "",
+});
+
 function App() {
   const [count, setCount] = useState(0)
 
