2e1c3d05ae3f3d0a32f4eccb2a4b26980ae68957
jer <32397605+jerxmyg@users.noreply.github.com>
2023-03-27 22:56:19 -0400

    Step 2.

--- crypto-hustle/src/App.jsx
+++ crypto-hustle/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect  } from 'react'
+import CoinInfo from "./Components/coinInfo";
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -11,8 +12,8 @@ function App() {
       const response = await fetch(
         "https://min-api.cryptocompare.com/data/all/coinlist?&api_key" 
         + API_KEY);
-      const json = await response.json();
-      setList(json);
+        const json = await response.json();
+        setList(json);
     };
     fetchAllCoinData().catch(console.error);
 
@@ -24,7 +25,13 @@ function App() {
       <ul>
       {list && Object.entries(list.Data).map(([coin]) =>
     list.Data[coin].PlatformType === "blockchain" ? (
-      <li key={list.Data[coin].FullName}>{list.Data[coin].FullName}</li>
+
+      <CoinInfo
+      image={list.Data[coin].ImageUrl}
+      name={list.Data[coin].FullName}
+      symbol={list.Data[coin].Symbol}
+      />
+      
     ) : null
   )}
       </ul>
