d02928a972b7c99ecb456e61f2b1de241d6ac9f3
jer <32397605+jerxmyg@users.noreply.github.com>
2023-03-27 21:25:05 -0400

    Step 1.

--- crypto-hustle/src/App.jsx
+++ crypto-hustle/src/App.jsx
@@ -1,15 +1,37 @@
-import { useState } from 'react'
+import { useState, useEffect  } from 'react'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [list, setList] = useState(null)
+  useEffect(() => {
+    const fetchAllCoinData = async () => {
+      const response = await fetch(
+        "https://min-api.cryptocompare.com/data/all/coinlist?&api_key" 
+        + API_KEY);
+      const json = await response.json();
+      setList(json);
+    };
+    fetchAllCoinData().catch(console.error);
+
+  }, []);
 
   return (
+    <div className="whole-page">
+      <h1>Crypto List</h1>
+      <ul>
+      {list && Object.entries(list.Data).map(([coin]) =>
+    list.Data[coin].PlatformType === "blockchain" ? (
+      <li key={list.Data[coin].FullName}>{list.Data[coin].FullName}</li>
+    ) : null
+  )}
+      </ul>
+    </div>
     
   )
+  
 }
 
 export default App
